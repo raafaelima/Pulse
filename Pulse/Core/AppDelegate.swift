e27bc88e390c9572696d378829946d3b2e799c1d
Rafael Lima <rafael.lima2403@gmail.com>
2023-01-13 17:17:23 +0000

    First Commit

---
+++ Pulse/Core/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Pulse
+//
+//  Created by Rafael Lima on 13/01/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
